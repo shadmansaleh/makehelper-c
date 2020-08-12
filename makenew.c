14e6d3713f5fe1f43e2e8d0898cb909ccaf49a13
shadmansaleh <shadmansaleh3@gmail.com>
2020-08-12 16:21:14 +0600

    Changed Makefile Templet
    -Added install and uninstall rule for Makefile
        uses $PREFIX/local for instalation
    -changed the structure how Makefile template is stored to increase
    readability

--- makenew.c
+++ makenew.c
@@ -3,8 +3,38 @@
 
 void create_new(val v){
 	FILE *make=fopen("Makefile","w");
-	fprintf(make,"MAIN = %s\nSRCS = %s\nINCLUDES = %s\nLIBS = %s\nCFLAGS = %s\nLFLAGS = %s\nCC = %s\nOBJS = $(SRCS:%s=.o)\n\n#End of variables\n\n",v.MAIN,v.SRCS,v.INCLUDES,v.LIBS,v.CFLAGS,v.LFLAGS,v.CC,v.EXT);
-	fprintf(make,".PHONY: depend clean\nall:\t$(MAIN)\n\t@echo  $(MAIN) has been compiled\n$(MAIN):\t$(OBJS)\n\t$(CC) $(CFLAGS) $(INCLUDES) -o $(MAIN) $(OBJS) $(LFLAGS) $(LIBS)\n%s.o:\n\t$(CC) $(CFLAGS) $(INCLUDES) -c $<  -o $@\nclean:\n\t$(RM) *.o *~ $(MAIN)\nrun :\t$(MAIN)\n\t./$(MAIN)\ndepend:\t$(SRCS)\n\tmakedepend $(INCLUDES) $^\n\n# DO NOT DELETE THIS LINE -- make depend needs it\n\n",v.EXT);
+	fprintf(make,"MAIN = %s\n"\
+            "SRCS = %s\nINCLUDES = %s\n"\
+            "LIBS = %s\n"\
+            "CFLAGS = %s\n"\
+            "LFLAGS = %s\n"\
+            "CC = %s\n"\
+            "OBJS = $(SRCS:%s=.o)\n"\
+            "\n"\
+            "#End of variables\n"\
+            "\n"\
+            "",v.MAIN,v.SRCS,v.INCLUDES,v.LIBS,v.CFLAGS,v.LFLAGS,v.CC,v.EXT);
+	fprintf(make,".PHONY: depend clean\n"\
+            "all:\t$(MAIN)\n"\
+               "\t@echo  $(MAIN) has been compiled\n"\
+            "$(MAIN):\t$(OBJS)\n"\
+                "\t$(CC) $(CFLAGS) $(INCLUDES) -o $(MAIN) $(OBJS) $(LFLAGS) $(LIBS)\n"\
+            "%s.o:\n"\
+                "\t$(CC) $(CFLAGS) $(INCLUDES) -c $<  -o $@\n"\
+            "clean:\n"\
+                "\t$(RM) *.o *~ $(MAIN)\n"\
+            "run :\t$(MAIN)\n"\
+                "\t./$(MAIN)\n"\
+            "install :\t$(MAIN)\n"\
+                "\tcp $(MAIN) $(PREFIX)/local/bin\n"\
+            "uninstall :\t$(PREFIX)/local/bin/$(MAIN)\n"\
+                "\trm -i $(PREFIX)/local/bin/$(MAIN)\n"\
+            "depend:\t$(SRCS)\n"\
+                "\tmakedepend $(INCLUDES) $^\n"\
+            "\n"\
+            "# DO NOT DELETE THIS LINE -- make depend needs it\n"\
+            "\n"\
+           "",v.EXT);
 	fclose(make);
 	return;
 }
